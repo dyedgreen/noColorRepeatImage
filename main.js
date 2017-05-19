4a9fe64754c4ce4952e2cf0e5ed0092d312149dc
dyedgreen <dyedgreen@Tilmans-iMac.local>
2017-05-19 22:03:12 +0200

    Added some caching, improved color-indexing

--- main.js
+++ main.js
@@ -13,95 +13,137 @@
 * that matches an (arbitrary) condition.
 * This uses a very naive search approach
 * and can be very slow.
+* The cache assumes, that each color can only be further away than the last.
 * NOTE:
 * l = limit; f = force; d = decision; o = offset;
 *
 * @param {object} color (Format: {r,g,b})
 * @param {function} test (takes the new color as {r,g,b} has to retunr true/false)
+* @param {bool} cache
 * @return {objectORbool} (Returns {r,g,b}. Returns false, if no color is found.)
 */
-function searchColor(color, test) {
-  // Search the rgb color space in around color
+searchColorCacheC = [];
+searchColorCacheR = [];
+searchColorCacheG = [];
+searchColorCacheB = [];
+function searchColor(color, test, cache) {
+  // Set up the search
   var lR = 255 - color.r, fR = 0, dR = 0, oR = 0, r = 0;
   var lG = 255 - color.g, fG = 0, dG = 0, oG = 0, g = 0;
   var lB = 255 - color.b, fB = 0, dB = 0, oB = 0, b = 0;
   var newColor = {r:0,g:0,b:0};
-  for (r = 1; r < 257; r ++) {
-  for (g = 1; g < 257; g ++) {
-  for (b = 1; b < 257; b ++) {
-    // Generate the offsets
-    // R
-    if (dR === 1) {
-      fR = r + oR;
-    } else if (dR === 2) {
-      fR = oR - r;
-    } else if (r % 2 === 0) {
-      fR = r/2;
-      if (fR > lR) {
-        fR = -fR;
-        dR = 2; // Overflow top (offset is already set neg!)
-        oR = r/2;
-      }
-    } else {
-      fR = (r-1)/2;
-      if (fR > color.r) {
-        fR ++;
-        dR = 1; // Overflow bottom
-        oR = (1-r) / 2;
-      } else {
-        fR = -fR;
+  // Perform cache lookup
+  var cacheI = -1, cacheV = 0, cR = 1, cG = 1, cB = 1;
+  if (cache) {
+    cacheV = color.r + 1000 * color.g + 1000000 * color.b;
+    for (var i = 0, l = searchColorCacheC.length; i < l; i ++) {
+      if (cacheV === searchColorCacheC[i]) {
+        cacheI = i;
+        cR = searchColorCacheR[i];
+        cG = searchColorCacheG[i];
+        cB = searchColorCacheB[i];
+        break;
       }
     }
-    // G
-    if (dG === 1) {
-      fG = g + oG;
-    } else if (dG === 2) {
-      fG = oG - g;
-    } else if (g % 2 === 0) {
-      fG = g/2;
-      if (fG > lG) {
-        fG = -fG;
-        dG = 2;
-        oG = g/2;
-      }
-    } else {
-      fG = (g-1)/2;
-      if (fG > color.g) {
-        fG ++;
-        dG = 1;
-        oG = (1-g) / 2;
-      } else {
-        fG = -fG;
-      }
-    }
-    // B
-    if (dB === 1) {
-      fB = b + oB;
-    } else if (dB === 2) {
-      fB = oB - b;
-    } else if (b % 2 === 0) {
-      fB = b/2;
-      if (fB > lB) {
-        fB = -fB;
-        dB = 2;
-        oB = b/2;
-      }
-    } else {
-      fB = (b-1)/2;
-      if (fB > color.b) {
-        fB ++;
-        dB = 1;
-        oB = (1-b) / 2;
-      } else {
-        fB = -fB;
-      }
-    }
-    // Create the new color
-    newColor = { r: color.r+fR, g:color.g+fG, b: color.b+fB };
-    // Test the new color
-    if (test(newColor)) return newColor;
   }
-  }
+  // Search RGB color space
+  for (r = cR; r < 257; r ++) {
+    for (g = cG; g < 257; g ++) {
+      for (b = cB; b < 257; b ++) {
+        // Generate the offsets
+        // R
+        if (dR === 1) {
+          fR = r + oR;
+        } else if (dR === 2) {
+          fR = oR - r;
+        } else if (r % 2 === 0) {
+          fR = r/2;
+          if (fR > lR) {
+            fR = -fR;
+            dR = 2; // Overflow top (offset is already set neg!)
+            oR = r/2;
+          }
+        } else {
+          fR = (r-1)/2;
+          if (fR > color.r) {
+            fR ++;
+            dR = 1; // Overflow bottom
+            oR = (1-r) / 2;
+          } else {
+            fR = -fR;
+          }
+        }
+        // G
+        if (dG === 1) {
+          fG = g + oG;
+        } else if (dG === 2) {
+          fG = oG - g;
+        } else if (g % 2 === 0) {
+          fG = g/2;
+          if (fG > lG) {
+            fG = -fG;
+            dG = 2;
+            oG = g/2;
+          }
+        } else {
+          fG = (g-1)/2;
+          if (fG > color.g) {
+            fG ++;
+            dG = 1;
+            oG = (1-g) / 2;
+          } else {
+            fG = -fG;
+          }
+        }
+        // B
+        if (dB === 1) {
+          fB = b + oB;
+        } else if (dB === 2) {
+          fB = oB - b;
+        } else if (b % 2 === 0) {
+          fB = b/2;
+          if (fB > lB) {
+            fB = -fB;
+            dB = 2;
+            oB = b/2;
+          }
+        } else {
+          fB = (b-1)/2;
+          if (fB > color.b) {
+            fB ++;
+            dB = 1;
+            oB = (1-b) / 2;
+          } else {
+            fB = -fB;
+          }
+        }
+        // Create the new color
+        newColor = { r: color.r+fR, g:color.g+fG, b: color.b+fB };
+        // Test the new color
+        if (test(newColor)) {
+          // Write to cache if needed
+          if (cache && r > 0) {
+            if (cacheI === -1) {
+              searchColorCacheC.push(cacheV);
+              searchColorCacheR.push(r);
+              searchColorCacheG.push(g);
+              searchColorCacheB.push(b);
+            } else {
+              searchColorCacheC[cacheI] = cacheV;
+              searchColorCacheR[cacheI] = r;
+              searchColorCacheG[cacheI] = g;
+              searchColorCacheB[cacheI] = b;
+            }
+          }
+          // Return result
+          return newColor;
+        }
+      }
+      // Correct cache value for B
+      cB = 1;
+    }
+    // Correct cache value for G
+    cG = 1;
   }
   // No color was found, return false
   return false;
@@ -181,20 +223,22 @@ function filterCanvas(canvas, feedback, callback) {
     var newColor = searchColor(color, function(c) {
       // Test is color exists (using for loop)
       var l = used.length;
-      var cs = c.r + 100 * c.g + 10000 * c.b;
+      var cs = c.r + 1000 * c.g + 1000000 * c.b;
       for (var i = 0; i < l; i ++) {
         if (used[i] === cs) return false;
       }
       return true;
-    });
+    }, true);
     // Assign the new color
     if (newColor !== false) {
       // By giving each value a different magnitude, we don't need to convert to string
-      used.push(newColor.r + 100 * newColor.g + 10000 * newColor.b);
+      used.push(newColor.r + 1000 * newColor.g + 1000000 * newColor.b);
       pix[i] = newColor.r;
       pix[i+1] = newColor.g;
       pix[i+2] = newColor.b;
     } else {
+      // This should never be meet if the seach function works
+      console.log('Malformed search function.');
       i = pix.length;
     }
     // Next loop step
